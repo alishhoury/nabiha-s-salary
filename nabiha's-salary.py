e9d8c1b1b7760fdee29a94d2710933b4891a32e1
ALi Shhoury <alishhoury78@gmail.com>
2025-02-20 23:22:32 +0200

    trying to handle errors

--- nabiha's-salary.py
+++ nabiha's-salary.py
@@ -1,22 +1,31 @@
-salary = int(input("Please enter your salary for this month:"))
-month = str(input("Please enter the name of the month:"))
-savings = int(input("Please enter savings percentage:"))
-rent = int(input("Please input Rent percentage:"))
-electricity = int(input("Please input electricity percentage:"))
+def calculaions():
+    
+    if total < 100 :
+        savings = salary*(savings/100)
+        rent = salary*(rent/100)
+        electricity = salary*(electricity/100)
+        total = savings + rent + electricity
+        print(f"The amount allocated to savings is: {savings}")
+        print(f"The amount allocated to rent is: {rent}")
+        print(f"The amount allocated to electricity is: {electricity}")
+        print(f"The total amount spend on savings, rent, and electricity is: {total}")
+        print(f"The remainder of nabiha's salary is: {salary-total}")
+        print(f"Estimated yearly rent and electicity: {(rent*12)+(electricity*12)}")
+        print(f"Nabiha's salary squared:{salary**2}")
+        save = int(input("The additional saving:"))
+        print(f"Additional saving divided by total savings {save/savings}")
+
+month = None ; salary = 0 ; savings = 0 ; rent = 0 ; electricity = 0 ; varibles = [salary,savings,rent,electricity]
 total = savings + rent + electricity
-if total < 100 :
-    savings = salary*(savings/100)
-    rent = salary*(rent/100)
-    electricity = salary*(electricity/100)
-    total = savings + rent + electricity
-    print(f"The amount allocated to savings is: {savings}")
-    print(f"The amount allocated to rent is: {rent}")
-    print(f"The amount allocated to electricity is: {electricity}")
-    print(f"The total amount spend on savings, rent, and electricity is: {total}")
-    print(f"The remainder of nabiha's salary is: {salary-total}")
-    print(f"Estimated yearly rent and electicity: {(rent*12)+(electricity*12)}")
-    print(f"Nabiha's salary squared:{salary**2}")
-    save = int(input("The additional saving:"))
-    print(f"Additional saving divided by total savings {save/savings}")
-else:
-    print("percentage is wrong")
+d = {}
+while month != "exit":
+    month = str(input("Please enter the name of the month (type exit to quit):"))
+    if month != "exit" :  
+        salary = int(input("Please enter your salary for this month:"))
+        savings = int(input("Please enter savings percentage:"))
+        rent = int(input("Please input Rent percentage:"))
+        electricity = int(input("Please input electricity percentage:"))
+        if total > 100:
+            print("The combined percentage exceeds 100%. Please enter the values again.")
+            d[month] = varibles
+print(d)
